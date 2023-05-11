04931f40de38436a79313ed05e63b09b7445e15f
Alberto Cuogo <cuogoalberto@gmail.com>
2023-05-11 16:10:45 +0200

    laravel base

---
+++ LARAVEL03_Selfwork/resources/views/index.blade.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>AirTravel</title>
+</head>
+
+<body>
+
+    <div class="container d-flex justify-content-center">
+        <header class="d-flex  py-4 mb-4">
+            <img src="img/AirTravel-logo.png" class="bi me-2" height="80rem">
+
+            <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+
+                <span class="fs-1 fw-bold">AirTravel</span>
+            </a>
+        </header>
+    </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
